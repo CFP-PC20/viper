25f214366e4fccecbaaf89f04374290964daa479
joaquin <joaquin.corugedo@campusfp.es>
2019-01-30 19:36:48 +0100

    Añadir comida y desplazamiento paredes

--- scripts/game.js
+++ scripts/game.js
@@ -1,37 +1,59 @@
+var food;
+var speed = 10;
+
 function setup() {
-    createCanvas(600, 600);
+    
+    createCanvas(400, 400);
     serpiente = new Serpiente();
     cuerpo = new Cuerpo();
-    frameRate(10);
+    frameRate(5);
+    pickLocation();
 }
 
+function pickLocation(){
+    var cols=floor(width/10);
+    var rows=floor(height/10);   
+    food=createVector(floor(random(cols)) * 10, floor(random(rows)) * 10);    
+}
 
 function draw() {
     background(51);
     serpiente.update();
     serpiente.draw( serpiente.x, serpiente.y);
 
+    fill(255, 0, 100);
+    rect(food.x,food.y, 10, 10);
+
     cuerpo.update(); 
     cuerpo.draw();
+
+    update();
+}
+
+function update() {
+    if(serpiente.x == food.x && serpiente.y == food.y){
+        cuerpo.add();
+        pickLocation();
+    }
 }
 
 function keyPressed() {
     switch (keyCode) {
     case DOWN_ARROW:
-        serpiente.dir(0, 10);
+        serpiente.dir(0, speed);
         break;
     case UP_ARROW:
-        serpiente.dir(0, -10);
+        serpiente.dir(0, -speed);
         break;
     case RIGHT_ARROW:
-        serpiente.dir(10, 0);
+        serpiente.dir(speed, 0);
         break;
     case LEFT_ARROW:
-        serpiente.dir(-10, 0);
+        serpiente.dir(-speed, 0);
         break;
     case 77:
         //letra m. eliminar y llevar contenido a la funcion que detecta si la serpiente esta sobre la comida
-	cuerpo.add();
+	    cuerpo.add();
         break;
     }
 }
--- scripts/serpiente.js
+++ scripts/serpiente.js
@@ -10,13 +10,28 @@ function Serpiente() {
     }
 
     this.update = function () {
+        if (this.x<0){
+            this.x=width;
+        }else if (this.x>width){
+            this.x=0;
+        }
+        if (this.y<0){
+            this.y=height;
+        }else if (this.y>height){
+            this.y=0;
+        }
+        
         this.x = this.x + this.xSpeed;
         this.y = this.y + this.ySpeed;
+        console.log("("+ this.x + "," + this.y + ")")
+
     }
 
     this.draw = function ( drawx, drawy ) {
         fill(255);
         rect(drawx, drawy, 10, 10);
+
+        
     }
 
 }
